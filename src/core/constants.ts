9359cf587d723f504540330623aef5b2d71cb27c
Max Knivets <knivets.m@gmail.com>
2023-10-10 00:30:22 -0400

    feat: KB

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,3 +1,5 @@
 export const MEM_URL_READ = "https://mem-api.com/state/";
 export const MEM_URL_WRITE = "https://mem-api.com/transactions/";
 export const MEM_URL_TESTNET = "https://mem-testnet-bfdc8ff3530f.herokuapp.com/";
+
+export const KB = 1024 * 1024;
